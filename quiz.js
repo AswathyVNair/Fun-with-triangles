b21da85b27d98591c5a826f15a85300ccae563a0
AswathyVNair <88492274+AswathyVNair@users.noreply.github.com>
2021-09-30 19:38:01 +0530

    Create quiz.js

---
+++ quiz.js
@@ -0,0 +1,21 @@
+const quizForm = document.querySelector(".quiz-form");
+const submitAnswerbtn = document.querySelector("#submit-answer-btn");
+const outputEl = document.querySelector("#output");
+
+const correctAnswers = ["90°", "right angled"];
+
+function calculateScore() {
+  let score = 0;
+  let index = 0;
+  const formResults = new FormData(quizForm);
+
+  for (let value of formResults.values()) {
+    if (value === correctAnswers[index]) {
+      score = score + 1;
+    }
+    index = index + 1;
+  }
+  outputEl.innerText = "Your score is " + score;
+}
+
+submitAnswerbtn.addEventListener("click", calculateScore);
