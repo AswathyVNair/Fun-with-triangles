9b92e64dfcbd7470076d452f740947ad4a0c4e94
AswathyVNair <88492274+AswathyVNair@users.noreply.github.com>
2021-10-02 19:06:11 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const inputs = document.querySelectorAll(".angle-input");
+const isTriangleBtn = document.querySelector("#is-triangle-btn");
+const outputEl = document.querySelector("#output");
+
+function calculateSumOfAngles(angle1, angle2, angle3) {
+  const sumOfAngles = angle1 + angle2 + angle3;
+  return sumOfAngles;
+}
+function isTriangle() {
+  const sumOfangles = calculateSumOfAngles(
+    Number(inputs[0].value),
+    Number(inputs[1].value),
+    Number(inputs[2].value)
+  );
+  if (sumOfangles === 180) {
+    outputEl.innerText = "Yay! The angles form a triangle";
+  } else {
+    outputEl.innerText = "Oh Oh! The angles don't form a triangle";
+  }
+}
+
+isTriangleBtn.addEventListener("click", isTriangle);
