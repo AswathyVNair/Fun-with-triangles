9082e27c6744abce6d4ac30fe21500c5bda6391a
AswathyVNair <88492274+AswathyVNair@users.noreply.github.com>
2021-10-12 14:29:59 +0530

    feat : error handling

--- area.js
+++ area.js
@@ -1,5 +1,5 @@
 const sides = document.querySelectorAll(".side-input");
-const CheckAreaBtn = document.querySelector("#area-btn");
+const checkAreaBtn = document.querySelector("#area-btn");
 const outputEl = document.querySelector("#output");
 
 function calculateArea(base, height) {
@@ -12,7 +12,14 @@ function checkArea() {
     Number(sides[0].value),
     Number(sides[1].value)
   );
-  outputEl.innerText = "Area of triangle is " + triangleArea + "cm²";
+
+  if (sides[0].value === "" || sides[1].value === "") {
+    outputEl.innerText = "Please enter values in the given fields";
+  } else if (Number(sides[0].value) <= 0 || Number(sides[1].value) <= 0) {
+    outputEl.innerText = "Enter positive values in the given fields";
+  } else {
+    outputEl.innerText = "Area of triangle is " + triangleArea + "cm²";
+  }
 }
 
-CheckAreaBtn.addEventListener("click", checkArea);
+checkAreaBtn.addEventListener("click", checkArea);
