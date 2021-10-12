379c492e5ab1e5b62cdb4b3713d7cadb2112c2ea
AswathyVNair <88492274+AswathyVNair@users.noreply.github.com>
2021-10-12 14:29:18 +0530

    feat : error handling

--- index.js
+++ index.js
@@ -12,7 +12,20 @@ function isTriangle() {
     Number(inputs[1].value),
     Number(inputs[2].value)
   );
-  if (sumOfangles === 180) {
+
+  if (
+    inputs[0].value === "" ||
+    inputs[1].value === "" ||
+    inputs[2].value === ""
+  ) {
+    outputEl.innerText = "Please enter values in the given fields";
+  } else if (
+    Number(inputs[0].value) <= 0 ||
+    Number(inputs[1].value) <= 0 ||
+    Number(inputs[2].value) <= 0
+  ) {
+    outputEl.innerText = "Enter positive values in the given fields";
+  } else if (sumOfangles === 180) {
     outputEl.innerText = "Yay! The angles form a triangle";
   } else {
     outputEl.innerText = "Oh Oh! The angles don't form a triangle";
